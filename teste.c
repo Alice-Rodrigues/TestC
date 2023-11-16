e83c30565cc308baa0ab32c1263e9015f04e0e42
Alice00101101Rodrigues <54628916+Alice-Rodrigues@users.noreply.github.com>
2023-11-16 18:22:04 +0000

    exemplo de teste em c

---
+++ teste.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int soma(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    assert(soma(2,3) == 5);
+    assert(soma(4,10) == 14);
+    assert(soma(2,2) != 5);
+    return EXIT_SUCCESS;
+}
